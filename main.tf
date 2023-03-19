ad84a40180d69b2457c24238812652f3822ae4bf
Christian Nuss <christian.nuss@gmail.com>
2023-03-19 15:04:57 -0700

    set content types

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
+locals {
+  # Credit: https://engineering.statefarm.com/blog/terraform-s3-upload-with-mime/
+  mime_types = jsondecode(file("${path.module}/mime-types.json"))
+}
+
 resource "aws_s3_bucket_object" "object" {
   for_each = fileset(var.dist_dir, "**/*")
 
   bucket = var.bucket_name
   key    = each.value
   source = "${var.dist_dir}/${each.value}"
+
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
 
   etag = filemd5("${var.dist_dir}/${each.value}")
 }
